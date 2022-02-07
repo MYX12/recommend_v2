2e26ac66207e280e9a2588a96ee822c2d920cade
MYX12 <92570565+MYX12@users.noreply.github.com>
2022-02-07 15:33:59 +0800

    Update recommendation.py

--- recommendation.py
+++ recommendation.py
@@ -1,8 +1,5 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from zipfile import ZipFile
-import seaborn as sns
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
